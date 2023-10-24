fbf11e8a78f9858e65f5a95ac93160730e6bb101
Jacob Bergdahl <jacob_bergdahl@live.se>
2023-10-24 12:51:53 +0200

    Improve error handling for PaLM

--- pages/api/aiModels/palm.ts
+++ pages/api/aiModels/palm.ts
@@ -26,26 +26,27 @@ export const palm = async (
 ) => {
   console.log(`The backend is calling Google PaLM model ${model}.`);
 
-  const response =
-    model === MODEL.PalmTextBison001
-      ? await textServiceClient.generateText({
-          model: "models/text-bison-001",
-          temperature: temperature,
-          prompt: {
-            text: message,
-          },
-        })
-      : await chatServiceClient.generateMessage({
-          model: "models/chat-bison-001",
-          temperature: temperature,
-          candidateCount: 1,
-          prompt: {
-            // This is not fully implemented yet. We should send all
-            // messages in memory individually here.
-            // optional, preamble context to prime responses
-            /* context: "Respond to all questions with a rhyming poem.", */
-            // Optional. Examples for further fine-tuning of responses.
-            /* examples: [
+  try {
+    const response =
+      model === MODEL.PalmTextBison001
+        ? await textServiceClient.generateText({
+            model: "models/text-bison-001",
+            temperature: temperature,
+            prompt: {
+              text: message,
+            },
+          })
+        : await chatServiceClient.generateMessage({
+            model: "models/chat-bison-001",
+            temperature: temperature,
+            candidateCount: 1,
+            prompt: {
+              // This is not fully implemented yet. We should send all
+              // messages in memory individually here.
+              // optional, preamble context to prime responses
+              /* context: "Respond to all questions with a rhyming poem.", */
+              // Optional. Examples for further fine-tuning of responses.
+              /* examples: [
               {
                 input: { content: "What is the capital of California?" },
                 output: {
@@ -54,31 +55,38 @@ export const palm = async (
                 },
               },
             ], */
-            // Required. Alternating prompt/response messages.
-            messages: [{ content: message }],
-          },
-        });
+              // Required. Alternating prompt/response messages.
+              messages: [{ content: message }],
+            },
+          });
 
-  if (
-    model === MODEL.PalmTextBison001 &&
-    "output" in response[0].candidates[0]
-  ) {
-    const output = response[0].candidates[0].output;
-    SHOULD_SHOW_ALL_LOGS && console.log("Response from Google:", output);
+    if (
+      model === MODEL.PalmTextBison001 &&
+      "output" in response[0].candidates[0]
+    ) {
+      const output = response[0].candidates[0].output;
+      SHOULD_SHOW_ALL_LOGS && console.log("Response from Google:", output);
 
-    res.status(STATUS_CODE.Ok).json({ result: parseTextResponse(output) });
+      res.status(STATUS_CODE.Ok).json({ result: parseTextResponse(output) });
+      return;
+    } else if (
+      model === MODEL.PalmChatBison001 &&
+      "content" in response[0].candidates[0]
+    ) {
+      const output = response[0].candidates[0].content;
+      SHOULD_SHOW_ALL_LOGS && console.log("Response from Google:", output);
+
+      res.status(STATUS_CODE.Ok).json({ result: parseTextResponse(output) });
+      return;
+    }
+
+    res.status(STATUS_CODE.InternalServerError).json({ result: response });
     return;
-  } else if (
-    model === MODEL.PalmChatBison001 &&
-    "content" in response[0].candidates[0]
-  ) {
-    const output = response[0].candidates[0].content;
-    SHOULD_SHOW_ALL_LOGS && console.log("Response from Google:", output);
-
-    res.status(STATUS_CODE.Ok).json({ result: parseTextResponse(output) });
+  } catch (error) {
+    console.error("An error occurred:", error);
+    res
+      .status(STATUS_CODE.InternalServerError)
+      .json({ error: { message: error?.details || error } });
     return;
   }
-
-  res.status(STATUS_CODE.InternalServerError).json({ result: response });
-  return;
 };
